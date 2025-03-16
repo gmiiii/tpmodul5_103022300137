60b821e25a74161cc8ea62b226ed10e645e681cc
gmii <gumilarharisubagja28@gmail.com>
2025-03-16 19:36:49 +0700

    finished genericclass

--- tpmodul5_103022300137/Program.cs
+++ tpmodul5_103022300137/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        DataGeneric<long> dataInt = new DataGeneric<long>(103022300137);
+        dataInt.tampilData();
+    }
+}
+
+public class DataGeneric<T>
+{
+    public T data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void tampilData()
+    {
+        Console.WriteLine("Data: " + data);
     }
 }
