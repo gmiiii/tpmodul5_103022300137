74c53fb5e3ff7fd196a943738c649a5a9bc4de6f
gmii <gumilarharisubagja28@gmail.com>
2025-03-16 19:58:27 +0700

    Refactor: Move DataGeneric class to Class1.cs
    Reorganized code by relocating the DataGeneric<T> class from Program.cs to Class1.cs. This change enhances code organization and readability while keeping the Main method intact, which still creates an instance of DataGeneric<long> and calls the tampilData method

---
+++ tpmodul5_103022300137/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103022300137
+{
+    class DataGeneric<T>
+    {
+        public T data { get; set; }
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void tampilData()
+        {
+            Console.WriteLine("Data: " + data);
+        }
+    }
+}
--- tpmodul5_103022300137/Program.cs
+++ tpmodul5_103022300137/Program.cs
@@ -1,26 +1,13 @@
 ﻿using System;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
+
+namespace tpmodul5_103022300137;
 public class Program
 {
     public static void Main(string[] args)
     {
         DataGeneric<long> dataInt = new DataGeneric<long>(103022300137);
         dataInt.tampilData();
-    }
-}
-
-public class DataGeneric<T>
-{
-    public T data { get; set; }
-
-    public DataGeneric(T data)
-    {
-        this.data = data;
-    }
-
-    public void tampilData()
-    {
-        Console.WriteLine("Data: " + data);
     }
 }
