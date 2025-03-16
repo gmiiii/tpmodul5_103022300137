0bbfd1e8217ef10391427c3e95ca7ca5f93a409e
gmii <gumilarharisubagja28@gmail.com>
2025-03-16 19:31:00 +0700

    finished gneric method

--- tpmodul5_103022300137/Program.cs
+++ tpmodul5_103022300137/Program.cs
@@ -7,3 +7,11 @@ public class Program
         Console.WriteLine("Hello World!");
     }
 }
+
+public class HaloGeneric
+{
+    public void sapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo, " + user);
+    }   
+}
