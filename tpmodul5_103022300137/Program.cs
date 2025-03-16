9a226643af09084e6433751101654a23c772c140
gmii <gumilarharisubagja28@gmail.com>
2025-03-16 20:04:12 +0700

    Remove HaloGeneric class and related functionality
    The `Main` method in the `Program` class has been updated to eliminate the instantiation of the `HaloGeneric` class and the call to its `sapaUser` method. This removes the functionality that greeted a user with the name "Gumilar Hari Subagja". The `HaloGeneric` class and its `sapaUser` method have been completely removed from the codebase.

--- tpmodul5_103022300137/Program.cs
+++ tpmodul5_103022300137/Program.cs
@@ -4,15 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        HaloGeneric halo = new HaloGeneric();
-        halo.sapaUser("Gumilar Hari Subagja");
-    }
-}
 
-public class HaloGeneric
-{
-    public void sapaUser<T>(T user)
-    {
-        Console.WriteLine("Halo, " + user);
-    }   
+    }
 }
