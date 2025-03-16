6d8cd7b3cefd462be22d215334d5b9e663072687
gmii <gumilarharisubagja28@gmail.com>
2025-03-16 20:51:31 +0700

    Refactor Program and DataGeneric classes
    Removed namespace declaration and simplified the Program class by integrating the Main method. Restructured DataGeneric class to enhance readability by eliminating unnecessary braces and improving formatting. Overall, the code is now cleaner and more organized.

--- tpmodul5_103022300137/Program.cs
+++ tpmodul5_103022300137/Program.cs
@@ -1,27 +1,25 @@
 ﻿using System;
 
 
-namespace tpmodul5_103022300137
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            DataGeneric<long> dataInt = new DataGeneric<long>(103022300137);
-            dataInt.tampilData();
-        }
-    }
-
-    class DataGeneric<T>
-    {
-        public T data { get; set; }
-        public DataGeneric(T data)
-        {
-            this.data = data;
-        }
-        public void tampilData()
-        {
-            Console.WriteLine("Data: " + data);
-        }
+        DataGeneric<long> dataInt = new DataGeneric<long>(103022300137);
+        dataInt.tampilData();
     }
 }
+
+class DataGeneric<T>
+{
+    public T data { get; set; }
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void tampilData()
+    {
+        Console.WriteLine("Data: " + data);
+    }
+}
+
